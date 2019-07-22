97ae335244a0754432784541d0c4bfe2902988de
daniel <daapinzonch@unal.edu.co>
2019-07-22 10:23:05 -0500

    update
    update files

--- Practica spring boot/src/main/java/com/redcampo/app/constants/ErrorMesages.java
+++
@@ -1,8 +0,0 @@
-package com.redcampo.app.constants;
-
-public class ErrorMesages {
-
-    public static final String noExists = "Record Isn't in database";
-    public static final String alreadyExists = "CC is on dataBase";
-    public static final String unknow = "Quien sabe v:";
-}
